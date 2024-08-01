0da77a36544f3071648a584a7354058ca5180831
ingabireol <ingabireo64@gmail.com>
2024-08-01 22:43:30 +0200

    Adding loggings

---
+++ SecondProject/init.sql
@@ -0,0 +1,6 @@
+-- init.sql
+
+CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    lname VARCHAR(50) NOT NULL
+);
--- SecondProject1/src/main/java/servlet/MyServlet.java
+++ SecondProject1/src/main/java/servlet/MyServlet.java
@@ -9,13 +9,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 
 /**
  * Servlet implementation class MyServlet
@@ -27,8 +24,6 @@ public class MyServlet extends HttpServlet {
     /**
      * Default constructor. 
      */
-	private static final Logger logger = LogManager.getLogger(MyServlet.class);
-	
     public MyServlet() {
         // TODO Auto-generated constructor stub
     }
@@ -39,22 +34,18 @@ public class MyServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 //		String name = "Ingabire Olivier";
-		logger.info("Entered get Method");
 		String theId = request.getParameter("id");
 		if(theId == null || !theId.matches("\\d+")) {
 			response.sendRedirect("studentForm.html");
-			logger.error("Id is invalid");
 			response.getWriter().print("<h1>Id Invalid</h1>");
 			return;
 		}		
 		Integer id = Integer.parseInt(theId);
 		try {
-			String db_url = "jdbc:postgresql://localhost:5432/auca";
+			String db_url = "jdbc:postgresql://host.docker.internal:5432/auca";
 			String username = "postgres";
-			String password = "078868";	
-	
+			String password = "078868";			
 			Class.forName("org.postgresql.Driver");
-			logger.info("Loaded postgresql driver");
 			Connection con = DriverManager.getConnection(db_url,username,password);
 			
 			PreparedStatement pst =con.prepareStatement("select * from students where id = ?");
@@ -66,21 +57,18 @@ public class MyServlet extends HttpServlet {
 			if(rs.next()) {
 				String lname = rs.getString("lname");
 //				int id = rs.getInt(1);
-				logger.info("The name is "+lname+" and Id is "+id);
 				response.getWriter().print("<h1>The name is "+lname+" and Id is "+id+"</h1>");
 			}
 			else {
-				logger.info("Id entered does not exist");
 				response.getWriter().print("<h1>Id does not exist</h1>");
 			}
 			
 		} catch (SQLException e) {
-			logger.error("SQL exceptions caought : connection to db failed: "+e);
+			response.getWriter().print(e);
 			e.printStackTrace(response.getWriter());
 		} catch (ClassNotFoundException e) {
-			logger.error("Some class was not found"+e);
 			response.getWriter().print(e);
-//			e.printStackTrace(response.getWriter());
+			e.printStackTrace(response.getWriter());
 		}
 	}
 
@@ -89,11 +77,9 @@ public class MyServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		logger.info("Entered Post Method");
 		String name = request.getParameter("name");
 		String id = request.getParameter("id");
 		if(!id.matches("\\d+")) {
-			logger.warn("Entered an invalid id");
 			response.getWriter().print("<h1>Id must be an integer</h1>");
 			response.sendRedirect("studentForm.html");
 //			response.sendRedirect(request.getContextPath());
@@ -111,21 +97,18 @@ public class MyServlet extends HttpServlet {
 				pst.setInt(1, theId);
 				pst.setString(2, name);
 				int rowsAffected = pst.executeUpdate();
-				if(rowsAffected !=0) {		
-					logger.info("Inserted data in databse");
+				if(rowsAffected !=0) {					
 					response.getWriter().print("<h1>Inserted success</h1>");
 				}
 				else {
-					logger.error("Insert in database falied");
 					response.getWriter().print("<h1>Inserted Failed</h1>");
 				}
-								
+				
+				
 			} catch (SQLException e) {
-				logger.error("SQL exceptions caought : connection to db failed: "+e);
-//				e.printStackTrace();
+				e.printStackTrace();
 			} catch (ClassNotFoundException e) {
-				logger.error("Some class was not found"+e);
-//				e.printStackTrace();
+				e.printStackTrace();
 			}
 		}
 		
